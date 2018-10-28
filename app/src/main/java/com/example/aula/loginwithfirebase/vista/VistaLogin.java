a62ec7edd8618edd560baddd10d12a8c92c6169d
leo <leo1994448@gmil.com>
2018-10-28 22:23:08 +0100

    trying to correct R

--- app/src/main/java/com/example/aula/loginwithfirebase/vista/VistaLogin.java
+++ app/src/main/java/com/example/aula/loginwithfirebase/vista/VistaLogin.java
@@ -9,6 +9,7 @@ public class VistaLogin extends AppCompatActivity implements IVistaLogin{
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-       // setContentView(R.layout.activity_login);
+        //trying to correct te R error
+      //  setContentView(R.layout.activity_login);
     }
 }
