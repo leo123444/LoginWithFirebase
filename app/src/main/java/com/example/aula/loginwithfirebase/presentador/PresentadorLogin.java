40fd58cb6361da76de152d9c77a46794d184ae53
leo <leo1994448@gmil.com>
2018-10-28 21:35:42 +0100

    creating layouts an MVP model

---
+++ app/src/main/java/com/example/aula/loginwithfirebase/presentador/PresentadorLogin.java
@@ -0,0 +1,4 @@
+package com.example.aula.loginwithfirebase.presentador;
+
+public class PresentadorLogin {
+}
---
+++ app/src/main/java/com/example/aula/loginwithfirebase/vista/VistaLogin.java
@@ -0,0 +1,14 @@
+package com.example.aula.loginwithfirebase.vista;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+
+public class VistaLogin extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
